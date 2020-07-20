9fe175e07d17ee88bccc4d81a1d3e2114cdbcbae
Chester Heng <chester.heng@gmail.com>
2020-07-20 12:33:08 +0800

    Pagination - usePagingation hook

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
   - [Sorting - useSortBy hook](#sorting---usesortby-hook)
   - [Filtering - useFilters hook](#filtering---usefilters-hook)
   - [Sub Components - useExpanded hook](#sub-components---useexpanded-hook)
+  - [Pagination - usePagingation hook](#pagination---usepagingation-hook)
 
 ## Project Setup
 
@@ -486,4 +487,120 @@ const renderRowSubComponent = row => {
   renderRowSubComponent={renderRowSubComponent}
 />
 ```
+**[⬆ back to top](#table-of-contents)**
+
+## Pagination - usePagingation hook
+
+```javascript
+// TableContainer.js
+import { useTable, useSortBy, useFilters, useExpanded, usePagination } from 'react-table';
+import { Table, Row, Col, Button, Input, CustomInput } from "reactstrap"
+
+const {
+  getTableProps,
+  getTableBodyProps,
+  headerGroups,
+  // rows, -> we change 'rows' to 'page'
+  page,
+  prepareRow,
+  visibleColumns
+  // below new props related to 'usePagination' hook
+  canPreviousPage,
+  canNextPage,
+  pageOptions,
+  pageCount,
+  gotoPage,
+  nextPage,
+  previousPage,
+  setPageSize,
+  state: { pageIndex, pageSize }
+} = useTable(
+  {
+    columns,
+    data,
+    defaultColumn: { Filter: DefaultColumnFilter }
+    initialState: { pageIndex: 0, pageSize: 10 }
+  },
+  useFilters,
+  useSortBy,
+  useExpanded
+  usePagination
+);
+
+const onChangeInSelect = event => {
+  setPageSize(Number(event.target.value))
+}
+
+const onChangeInInput = event => {
+  const page = event.target.value ? Number(event.target.value) - 1 : 0
+  gotoPage(page)
+}
+
+  
+
+<Fragment>
+  <Table>{/* our table code here ... */}
+    <tbody {...getTableBodyProps()}>
+      {page.map(row => {
+        prepareRow(row);
+  </Table>
+
+  <Row style={{ maxWidth: 1000, margin: "0 auto", textAlign: "center" }}>
+    <Col md={3}>
+      <Button
+        color="primary"
+        onClick={() => gotoPage(0)}
+        disabled={!canPreviousPage}
+      >
+        {"<<"}
+      </Button>
+      <Button
+        color="primary"
+        onClick={previousPage}
+        disabled={!canPreviousPage}
+      >
+        {"<"}
+      </Button>
+    </Col>
+    <Col md={2} style={{ marginTop: 7 }}>
+      Page{" "}
+      <strong>
+        {pageIndex + 1} of {pageOptions.length}
+      </strong>
+    </Col>
+    <Col md={2}>
+      <Input
+        type="number"
+        min={1}
+        style={{ width: 70 }}
+        max={pageOptions.length}
+        defaultValue={pageIndex + 1}
+        onChange={onChangeInInput}
+      />
+    </Col>
+    <Col md={2}>
+      <CustomInput type="select" value={pageSize} onChange={onChangeInSelect}>
+        {[10, 20, 30, 40, 50].map(pageSize => (
+          <option key={pageSize} value={pageSize}>
+            Show {pageSize}
+          </option>
+        ))}
+      </CustomInput>
+    </Col>
+    <Col md={3}>
+      <Button color="primary" onClick={nextPage} disabled={!canNextPage}>
+        {">"}
+      </Button>
+      <Button
+        color="primary"
+        onClick={() => gotoPage(pageCount - 1)}
+        disabled={!canNextPage}
+      >
+        {">>"}
+      </Button>
+    </Col>
+  </Row>
+</Fragment>
+```
+
 **[⬆ back to top](#table-of-contents)**
--- src/TableContainer.js
+++ src/TableContainer.js
@@ -1,6 +1,12 @@
 import React, { Fragment } from "react"
-import { useTable, useSortBy, useFilters, useExpanded } from "react-table"
-import { Table } from 'reactstrap';
+import { 
+  useTable, 
+  useSortBy, 
+  useFilters, 
+  useExpanded, 
+  usePagination 
+} from "react-table"
+import { Table, Row, Col, Button, Input, CustomInput } from "reactstrap"
 import { Filter, DefaultColumnFilter } from './filters';
 
 const TableContainer = ({ columns, data, renderRowSubComponent }) => {
@@ -8,28 +14,49 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
     prepareRow,
-    visibleColumns
+    visibleColumns,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
   } = useTable(
     {
       columns,
       data,
-      defaultColumn: { Filter: DefaultColumnFilter }
+      defaultColumn: { Filter: DefaultColumnFilter },
+      initialState: { pageIndex: 0, pageSize: 10 }
     },
     useFilters,
     useSortBy,
-    useExpanded
+    useExpanded,
+    usePagination
   )
 
   const generateSortingIndicator = column => {
     return column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""
   }
   
+  const onChangeInSelect = event => {
+    setPageSize(Number(event.target.value))
+  }
+  
+  const onChangeInInput = event => {
+    const page = event.target.value ? Number(event.target.value) - 1 : 0
+    gotoPage(page)
+  }
+
   return (
     // If you're curious what props we get as a result of calling our getter functions (getTableProps(), getRowProps())
     // Feel free to use console.log()  This will help you better understand how react table works underhood.
-    <Table bordered hover {...getTableProps()}>
+    <Fragment>
+      <Table bordered hover {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
@@ -47,7 +74,7 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
       </thead>
 
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
+        {page.map(row => {
           prepareRow(row)
           return (
             <Fragment key={row.getRowProps().key}>
@@ -66,6 +93,62 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
         })}
       </tbody>
     </Table>
+      <Row style={{ maxWidth: 1000, margin: "0 auto", textAlign: "center" }}>
+<Col md={3}>
+  <Button
+    color="primary"
+    onClick={() => gotoPage(0)}
+    disabled={!canPreviousPage}
+  >
+    {"<<"}
+  </Button>
+  <Button
+    color="primary"
+    onClick={previousPage}
+    disabled={!canPreviousPage}
+  >
+    {"<"}
+  </Button>
+</Col>
+<Col md={2} style={{ marginTop: 7 }}>
+  Page{" "}
+  <strong>
+    {pageIndex + 1} of {pageOptions.length}
+  </strong>
+</Col>
+<Col md={2}>
+  <Input
+    type="number"
+    min={1}
+    style={{ width: 70 }}
+    max={pageOptions.length}
+    defaultValue={pageIndex + 1}
+    onChange={onChangeInInput}
+  />
+</Col>
+<Col md={2}>
+  <CustomInput type="select" value={pageSize} onChange={onChangeInSelect}>
+    {[10, 20, 30, 40, 50].map(pageSize => (
+      <option key={pageSize} value={pageSize}>
+        Show {pageSize}
+      </option>
+    ))}
+  </CustomInput>
+</Col>
+<Col md={3}>
+  <Button color="primary" onClick={nextPage} disabled={!canNextPage}>
+    {">"}
+  </Button>
+  <Button
+    color="primary"
+    onClick={() => gotoPage(pageCount - 1)}
+    disabled={!canNextPage}
+  >
+    {">>"}
+  </Button>
+</Col>
+</Row>
+    </Fragment>
   )
 }
 
